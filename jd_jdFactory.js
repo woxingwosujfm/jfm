b9dbba6a49f6d934cff6db8ef24bdb6bd8bb643d
woxingwosujfm <32244448+woxingwosujfm@users.noreply.github.com>
2021-03-28 23:12:01 +0800

    Add files via upload

--- jd_jdFactory.js
+++ jd_jdFactory.js
@@ -46,7 +46,7 @@ if ($.isNode()) {
 }
 let wantProduct = ``;//心仪商品名称
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-const inviteCodes = [``, ''];
+const inviteCodes = [`T0225KkcR0gf91LWdU-ilPQPdgCjVWnYaS5kRrbA`, 'T0225KkcR0gf91LWdU-ilPQPdgCjVWnYaS5kRrbA'];
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {
